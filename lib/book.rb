ea308480a0a57760248e3114fc55928a0c452825
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-11 17:49:41 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,8 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title, :genres
-  
-  Genres = []
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -12,5 +12,9 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+  # creates the writer for genre and adds the logic for the class constant
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
-
